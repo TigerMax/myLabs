1144c04d813ab900c5ccfe0674a3c97e27de90cb
Evgeniy Philippov <genya-genya2008@hotmail.com>
2015-10-26 08:32:08 +0400

    Начал Шеннона

---
+++ Shennon/Shenon/Shenon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Shenon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void readLetters(string filename)
+        {
+            try {
+                using (StreamReader stream = new StreamReader(filename))
+                {
+                    String line = stream.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Файл не удалось прочесть. Ошибка:");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
